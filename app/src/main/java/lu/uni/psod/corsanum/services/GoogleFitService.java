0f0bc17828b5f14e464cbd1dc12f1980e62fd48b
Rocio Lopez Perez <rocio.nightwater@gmail.com>
2015-12-16 15:41:46 +0100

    login/logout failure

--- app/src/main/java/lu/uni/psod/corsanum/services/GoogleFitService.java
+++ app/src/main/java/lu/uni/psod/corsanum/services/GoogleFitService.java
@@ -1,7 +1,11 @@
 package lu.uni.psod.corsanum.services;
 
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.IntentSender;
 import android.os.Bundle;
 import android.os.Handler;
@@ -29,24 +33,22 @@ public class GoogleFitService extends Service {
 
     public static final String SERVICE_REQUEST_TYPE = "requestType";
 
-    public static final int TYPE_GET_STEP_TODAY_DATA = 1;
     public static final int TYPE_REQUEST_CONNECTION = 2;
 
     public static final String HISTORY_INTENT = "fitHistory";
     public static final String HISTORY_EXTRA_STEPS_TODAY = "stepsToday";
 
     public static final String FIT_NOTIFY_INTENT = "fitStatusUpdateIntent";
+    public static final String FIT_LOGIN_LOGOUT_INTENT = "fitLoginLogoutIntent";
     public static final String FIT_EXTRA_CONNECTION_MESSAGE = "fitFirstConnection";
     public static final String FIT_EXTRA_NOTIFY_FAILED_STATUS_CODE = "fitExtraFailedStatusCode";
     public static final String FIT_EXTRA_NOTIFY_FAILED_INTENT = "fitExtraFailedIntent";
 
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        handleLoginLogout(intent);
         return START_STICKY;
     }
-
-
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -60,6 +62,10 @@ public class GoogleFitService extends Service {
             Log.d(TAG, "Disconecting Google Fit.");
             mGoogleApiFitnessClient.disconnect();
         }
+
+        Log.i(TAG, "Unregistering login/logout request reciever");
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mFitLoginLogoutRequestReceiver);
+
         super.onDestroy();
     }
 
@@ -68,6 +74,27 @@ public class GoogleFitService extends Service {
         super.onCreate();
         buildFitnessClient();
         Log.d(TAG, "GoogleFitService created");
+        LocalBroadcastManager
+                .getInstance(this)
+                .registerReceiver(mFitLoginLogoutRequestReceiver, new IntentFilter(GoogleFitService.FIT_LOGIN_LOGOUT_INTENT));
+    }
+
+    public void connectGoogleFit() {
+
+        //block until google fit connects.  Give up after 10 seconds.
+        if (!mGoogleApiFitnessClient.isConnected()) {
+            mTryingToConnect = true;
+            mGoogleApiFitnessClient.connect();
+
+            //Wait until the service either connects or fails to connect
+            while (mTryingToConnect) {
+                try {
+                    Thread.sleep(100, 0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private void buildFitnessClient() {
@@ -86,7 +113,6 @@ public class GoogleFitService extends Service {
                                 mTryingToConnect = false;
                                 Log.d(TAG, "Notifying the UI that we're connected.");
                                 notifyUiFitConnected();
-
                             }
 
                             @Override
@@ -128,4 +154,41 @@ public class GoogleFitService extends Service {
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 
+    private void handleLoginLogout(Intent intent) {
+        Log.i(TAG, "Received login/logout intent");
+
+        int type = intent.getIntExtra(SERVICE_REQUEST_TYPE, -1);
+
+        Thread task = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                //block until google fit connects.  Give up after 10 seconds.
+                if (!mGoogleApiFitnessClient.isConnected()) {
+                    mTryingToConnect = true;
+                    mGoogleApiFitnessClient.connect();
+
+                    //Wait until the service either connects or fails to connect
+                    while (mTryingToConnect) {
+                        try {
+                            Thread.sleep(100, 0);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+        task.start();
+    }
+
+    private BroadcastReceiver mFitLoginLogoutRequestReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get extra data included in the Intent
+            handleLoginLogout(intent);
+
+        }
+    };
 }
